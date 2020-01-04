6e8a633c6b3d8fd4dfa70a9484109e87c3384202
codeconcept <contact@codeconcept.fr>
2020-01-04 19:34:44 +0100

    Refactoring to use express router

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-const fs = require("fs");
 const expressLayouts = require("express-ejs-layouts");
-const filename = `messages-${Date.now()}.txt`;
-const writer = fs.createWriteStream(filename, { flags: "a" });
+
+const indexRouter = require("./routes/home");
+const menuRouter = require("./routes/menu");
+const contactRouter = require("./routes/contact");
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
@@ -13,61 +14,9 @@ app.use(expressLayouts);
 
 app.use(express.urlencoded({ extended: true }));
 
-const dishes = [
-  { id: 1, title: "Galette complète", price: 4.5 },
-  { id: 2, title: "Crêpe beurre sucre", price: 1.5 }
-];
-
-const coords = {
-  address: "1 Avenue Alphonse Legault, 35170 Bruz",
-  tel: "06-06-06-06-06",
-  email: "contact@creperie.bzh"
-};
-
-let messages = [];
-let previousMessagesLoaded = false;
-
-app.get("/", (req, res) => {
-  res.render("home", {});
-});
-
-app.get("/menu", (req, res) => {
-  res.render("menu", { dishes });
-});
-
-app.get("/contact", (req, res) => {
-  res.render("contact", { coords });
-});
-
-app.post("/contact", (req, res) => {
-  console.log("req.body", req.body);
-  const message = { message: req.body, date: new Date().toISOString() };
-  if (!previousMessagesLoaded) {
-    readMessages();
-  }
-  messages = [...messages, message];
-  console.log("messages", messages);
-  writer.write(JSON.stringify(messages), err => {
-    if (err) {
-      return res.status(500).send({ msg: "Error saving data " });
-    }
-  });
-  res.render("contact", { coords });
-});
-
-function readMessages() {
-  console.log("Loading previous messages");
-  const reader = fs.createReadStream(filename);
-  let fileContent = "";
-  reader.on("data", chunk => {
-    fileContent += chunk.toString();
-  });
-
-  reader.on("end", () => {
-    messages = JSON.parse(fileContent);
-    previousMessagesLoaded = true;
-  });
-}
+app.use("/", indexRouter);
+app.use("/menu", menuRouter);
+app.use("/contact", contactRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
