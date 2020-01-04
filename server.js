5e214607df31ef44eb886fd8bad4a99034bf25a1
codeconcept <contact@codeconcept.fr>
2020-01-04 17:34:07 +0100

    Retrieving old messages and adding new ones

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
-const writer = fs.createWriteStream("messages.txt");
+const filename = `messages-${Date.now()}.txt`;
+const writer = fs.createWriteStream(filename, { flags: "a" });
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
@@ -20,6 +21,7 @@ const coords = {
 };
 
 let messages = [];
+let previousMessagesLoaded = false;
 
 app.get("/", (req, res) => {
   res.render("home", {});
@@ -36,15 +38,32 @@ app.get("/contact", (req, res) => {
 app.post("/contact", (req, res) => {
   console.log("req.body", req.body);
   const message = { message: req.body, date: new Date().toISOString() };
+  if (!previousMessagesLoaded) {
+    readMessages();
+  }
   messages = [...messages, message];
+  console.log("messages", messages);
   writer.write(JSON.stringify(messages), err => {
     if (err) {
       return res.status(500).send({ msg: "Error saving data " });
     }
-    // console.log("JSON.stringify(messages)", JSON.stringify(messages));
   });
   res.render("contact", { coords });
 });
+
+function readMessages() {
+  console.log("Loading previous messages");
+  const reader = fs.createReadStream(filename);
+  let fileContent = "";
+  reader.on("data", chunk => {
+    fileContent += chunk.toString();
+  });
+
+  reader.on("end", () => {
+    messages = JSON.parse(fileContent);
+    previousMessagesLoaded = true;
+  });
+}
 
 const PORT = 3000;
 app.listen(PORT, () => {
